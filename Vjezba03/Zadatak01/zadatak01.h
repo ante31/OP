249082b378503da7e61927019f2dcd096d541a7a
ante31 <92745972+ante31@users.noreply.github.com>
2021-11-08 20:35:27 +0100

    Create zadatak01.h

---
+++ Vjezba03/Zadatak01/zadatak01.h
@@ -0,0 +1,14 @@
+#ifndef ZADATAK01_H
+#define ZADATAK01_H
+
+#include <vector>
+using namespace std;
+
+class zadatak01
+{
+public:
+	void vector_number(vector <int> &v1, int n);
+	void vector_interval(vector <int>& v1, int a, int b);
+	void vector_printout(vector <int> v1);
+};
+#endif
